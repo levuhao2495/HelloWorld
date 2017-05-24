6059e6d817ddfe89f84eda94346fe95bc3ae0710
levuhao2495 <levuhao2495@gmail.com>
2017-05-24 09:23:50 +0700

    add success

--- HelloWorld/src/Package1/Hello.java
+++ HelloWorld/src/Package1/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void displayGreeting(){
 		System.out.println("This is the first git application ");
 		System.out.println("Hello world !");
-		System.out.println("Hello world !");
+		System.out.println("This is success !");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
