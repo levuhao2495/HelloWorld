9b031d431d2503cc92346fd65e8c21eb7c9adc48
levuhao2495 <levuhao2495@gmail.com>
2017-05-24 09:27:32 +0700

    add output change

--- HelloWorld/src/Package1/Hello.java
+++ HelloWorld/src/Package1/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void displayGreeting(){
 		System.out.println("This is the first git application ");
 		System.out.println("Hello world !");
-		System.out.println("This is success !");
+		System.out.println("This is comment 1 !");
+		System.out.println("This is comment 2 !");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
