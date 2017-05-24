a28cef91abafbfdee4357f1e6f7b1f7e8759c3f0
levuhao2495 <levuhao2495@gmail.com>
2017-05-24 09:03:23 +0700

    Initial commit of HelloWorld

---
+++ HelloWorld/src/Package1/Hello.java
@@ -0,0 +1,14 @@
+package Package1;
+
+public class Hello {
+
+	public static void displayGreeting(){
+		System.out.println("This is the first git application ");
+		System.out.println("Hello world !");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("-----------");
+		displayGreeting();
+	}
+}
